77abb168e0e0d0f675dcaed99d755a92e1e84f74
Andrzej Cichecki <wrx71646@student.wroclaw.merito.pl>
2024-12-31 12:14:31 +0100

    LAB2 zad 1,2,3

---
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/MedicalTreatmentDao.java
@@ -0,0 +1,9 @@
+package com.jpacourse.persistence.dao;
+
+import com.jpacourse.persistence.entity.MedicalTreatmentEntity;
+import java.util.List;
+
+public interface MedicalTreatmentDao extends Dao<MedicalTreatmentEntity, Long>
+{
+    public List<MedicalTreatmentEntity> findByVisitId(Long visitId);
+}
