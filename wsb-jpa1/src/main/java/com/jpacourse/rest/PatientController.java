21beff94d2b8b81b5cb1d5ea3fa9c3ef4722a455
Adrian Okorski <aokorski@gmail.com>
2025-01-14 22:26:01 +0100

    - Poprawiony kod aplikacji LAB02 ( aplikacja się nie uruchamiała ) - Testy LAB03 ( oprócz wersjonowania i (OptimisticLock)

---
+++ wsb-jpa1/src/main/java/com/jpacourse/rest/PatientController.java
@@ -0,0 +1,40 @@
+package com.jpacourse.rest;
+
+import com.jpacourse.dto.PatientTO;
+import com.jpacourse.dto.VisitTO;
+import com.jpacourse.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/{id}")
+    public PatientTO getPatientById(@PathVariable Long id) {
+        return patientService.findById(id);
+    }
+
+    @GetMapping("/{id}/visits")
+    public ResponseEntity<List<VisitTO>> getPatientsVisits(@PathVariable Long id) {
+        return ResponseEntity.ok(patientService.findVisitsForPatient(id));
+    }
+
+    @GetMapping("/ln/{lastName}")
+    public ResponseEntity<List<PatientTO>> getPatientsVisits(@PathVariable String lastName) {
+        return ResponseEntity.ok(patientService.findPatientsByLastName(lastName));
+    }
+
+    @GetMapping("/nov/{numberOfVisits}")
+    public ResponseEntity<List<PatientTO>> getPatientsWithMoreThanVisits(@PathVariable Long numberOfVisits) {
+        return ResponseEntity.ok(patientService.findPatientsThatHadMoreVisitsThan(numberOfVisits));
+    }
+}
